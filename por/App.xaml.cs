7d5a83079596d47cd3301abb8383cf39bc8824ff
Usuario <Usuario@CPD03-M19>
2024-03-04 15:07:28 -0300

    Adicionar arquivos de projeto.

---
+++ por/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace por
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ por/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace por
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
